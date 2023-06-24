3d8d755da428e13a3cb47c60a661c16c97ee5ba0
xjh <934622645@qq.com>
2023-06-24 09:42:21 +0800

    [utils] add display module for show information

--- CipherSystem.srcs/sources_1/new/top.v
+++ CipherSystem.srcs/sources_1/new/top.v
@@ -3,8 +3,40 @@
 module top (
     input wire CLK100MHZ,
     input wire CPU_RESETN,
-    output test
+    output CA,
+    output CB,
+    output CC,
+    output CD,
+    output CE,
+    output CF,
+    output CG,
+    output DP,
+    output [7:0] AN
 );
-  assign test = !CLK100MHZ;
+  parameter CLK_FREQUENCY = 100_000_000;
+
+  // display 
+  wire [31:0] data = 32'hffffffff;
+  wire        en;
+  assign en = 1'b1;
+
+  display #(
+      .CLK_FREQUENCY(CLK_FREQUENCY)
+  ) display_inst (
+      .en  (en),
+      .clk (CLK100MHZ),
+      .rstn(CPU_RESETN),
+      .data(data),
+      .CA  (CA),
+      .CB  (CB),
+      .CC  (CC),
+      .CD  (CD),
+      .CE  (CE),
+      .CF  (CF),
+      .CG  (CG),
+      .DP  (DP),
+      .AN  (AN)
+  );
+
 
 endmodule
