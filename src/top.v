c9f4c1cd4482ccfb4a99cd160548f0aa8bd3f628
Xiang JiaHao <934622645@qq.com>
2023-07-30 07:53:39 +0800

    [craft] pass key schedule for low area version

--- src/top.v
+++ src/top.v
@@ -16,27 +16,27 @@ module top (
   parameter CLK_FREQUENCY = 100_000_000;
 
   // display 
-  wire [31:0] data = 32'hffffffff;
-  wire        en;
-  assign en = 1'b1;
+//   wire [31:0] data = 32'hffffffff;
+//   wire        en;
+//   assign en = 1'b1;
 
-  display #(
-      .CLK_FREQUENCY(CLK_FREQUENCY)
-  ) display_inst (
-      .en  (en),
-      .clk (CLK100MHZ),
-      .rstn(CPU_RESETN),
-      .data(data),
-      .CA  (CA),
-      .CB  (CB),
-      .CC  (CC),
-      .CD  (CD),
-      .CE  (CE),
-      .CF  (CF),
-      .CG  (CG),
-      .DP  (DP),
-      .AN  (AN)
-  );
+//   display #(
+//       .CLK_FREQUENCY(CLK_FREQUENCY)
+//   ) display_inst (
+//       .en  (en),
+//       .clk (CLK100MHZ),
+//       .rstn(CPU_RESETN),
+//       .data(data),
+//       .CA  (CA),
+//       .CB  (CB),
+//       .CC  (CC),
+//       .CD  (CD),
+//       .CE  (CE),
+//       .CF  (CF),
+//       .CG  (CG),
+//       .DP  (DP),
+//       .AN  (AN)
+//   );
 
 
 endmodule
